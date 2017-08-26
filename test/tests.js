feeb69aeb0056cd65d67be90d8d8b858a891bc9f
ShushakovM <maxim.shushakov@news.com.au>
2017-08-26 13:31:36 +0700

    Add mocha test for #toBase64 and #fromBase64 in Base64 encoding

--- test/tests.js
+++ test/tests.js
@@ -2,6 +2,7 @@ const assert = require('assert');
 const { List, find } = require('../find-in-list.js');
 const { convert } = require('../base-atlassian.js');
 const { lookAndSay } = require('../look-and-say.js');
+require('../base64-encoding.js');
 
 describe('Find in a list', function() {
   describe('#find', function() {
@@ -44,6 +45,37 @@ describe('Look And Say', function() {
     });
     it('should return `22` for 22, n=100', function() {
       assert.equal(lookAndSay(22, 100), '22');
+    });
+  });
+})
+
+describe('Base64 Encoding', function() {
+  describe('#toBase64', function() {
+    it('should return `Zg==` for `f`', function() {
+      assert.equal('f'.toBase64(), 'Zg==');
+    });
+    it('should return `Zm8=` for `fo`', function() {
+      assert.equal('fo'.toBase64(), 'Zm8=');
+    });
+    it('should return `Zm9v` for `foo`', function() {
+      assert.equal('foo'.toBase64(), 'Zm9v');
+    });
+    it('should return `dGhpcyBpcyBhIHN0cmluZw==` for `this is a string`', function() {
+      assert.equal('this is a string'.toBase64(), 'dGhpcyBpcyBhIHN0cmluZw==');
+    });
+  });
+  describe('#fromBase64', function() {
+    it('should return `f` for `Zg==`', function() {
+      assert.equal('Zg=='.fromBase64(), 'f');
+    });
+    it('should return `fo` for `Zm8=`', function() {
+      assert.equal('Zm8='.fromBase64(), 'fo');
+    });
+    it('should return `foo` for `Zm9v`', function() {
+      assert.equal('Zm9v'.fromBase64(), 'foo');
+    });
+    it('should return `this is a string` for `dGhpcyBpcyBhIHN0cmluZw==`', function() {
+      assert.equal('dGhpcyBpcyBhIHN0cmluZw=='.fromBase64(), 'this is a string');
     });
   });
 })
